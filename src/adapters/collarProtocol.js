05d2d010f06b9e20934fa5ff7d32257091e27c90
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-10-01 16:26:10 +0200

    create roll offer onchain (WIP)

--- src/adapters/collarProtocol.js
+++ src/adapters/collarProtocol.js
@@ -1,5 +1,11 @@
+
+
+const { DEADLINE_MINUTES } = require("../constants");
 const ERCC20_ABI = require("../constants/abi/ERC20");
 const { PROVIDER_NFT_ABI } = require("../constants/abi/ProviderNFT");
+const { TAKER_NFT_ABI } = require("../constants/abi/TakerNFT");
+const { ROLLS_ABI } = require("../constants/abi/Rolls");
+const { LOANS_ABI } = require("../constants/abi/Loans");
 const { getContractInstance, getWalletInstance } = require("./ethers");
 async function parseReceipt(receipt, contract) {
     if (receipt.logs && receipt.logs.length > 0) {
@@ -57,6 +63,90 @@ async function createOnchainOffer(callstrike, ltv, amount, duration, providerNFT
     return offerId
 
 }
+
+
+async function createOnchainRollOffer(proposal, rpcUrl) {
+    try {
+        console.log("creating roll offer")
+        // Get wallet instance
+        const wallet = await getWalletInstance(rpcUrl, process.env.PRIVATE_KEY);
+        // Get contract instance
+        const rollsContract = await getContractInstance(
+            rpcUrl,
+            proposal.rolls_contract_address,
+            ROLLS_ABI,
+            wallet
+        );
+
+        // Extract proposal terms
+        const {
+            taker_id,
+            roll_fee_amount,
+            roll_fee_delta_factor_bips,
+            min_price,
+            max_price,
+            min_to_provider,
+        } = proposal;
+        const deadlineDate = new Date();
+        deadlineDate.setMinutes(deadlineDate.getMinutes() + DEADLINE_MINUTES);
+        const deadline = Math.ceil(deadlineDate.getTime() / 1000)
+        console.log({ deadline, time: deadlineDate.getTime() })
+
+        // get providerNFT address and id 
+        const takerNFTContractAddress = await getTakerNFTContractAddressByLoansContractAddress(rpcUrl, proposal.loans_contract_address);
+        const takerContract = await getContractInstance(
+            rpcUrl,
+            takerNFTContractAddress,
+            TAKER_NFT_ABI,
+            wallet
+        )
+        const [providerNFTContractAddress, providerNFTId] = await takerContract.getPosition(taker_id);
+        // approve provider id to rolls 
+        console.log({ providerNFTContractAddress, providerNFTId })
+        const providerNFTContract = await getContractInstance(
+            rpcUrl,
+            providerNFTContractAddress,
+            PROVIDER_NFT_ABI,
+            wallet
+        );
+        const approvalTX = await providerNFTContract.approve(proposal.rolls_contract_address, providerNFTId);
+        await approvalTX.wait();
+        // Create roll offer on-chain
+        const tx = await rollsContract.createRollOffer(
+            taker_id,
+            roll_fee_amount,
+            roll_fee_delta_factor_bips,
+            min_price,
+            max_price,
+            min_to_provider,
+            deadline
+        );
+
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+        console.log({ receipt })
+        // Return the transaction receipt
+        return receipt;
+    } catch (error) {
+        console.error('Error creating roll offer on-chain:', error);
+        throw error;
+    }
+}
+
+async function getTakerNFTContractAddressByLoansContractAddress(
+    rpcUrl,
+    loansContractAddress
+) {
+    const loansContract = await getContractInstance(
+        rpcUrl,
+        loansContractAddress,
+        LOANS_ABI
+    )
+    const takerNFTContractAddress = await loansContract.takerNFT()
+    return takerNFTContractAddress
+}
+
 module.exports = {
-    createOnchainOffer
+    createOnchainOffer,
+    createOnchainRollOffer
 }
