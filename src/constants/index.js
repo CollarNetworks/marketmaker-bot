92b66b0571bd90d601dcc0b6417ef903b9fd6070
Carlos D. Di Matteo <carlosdima97@gmail.com>
2024-12-11 15:54:23 -0500

    updated adapters to new setup and contracts

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 // Load configuration from environment variables
-const API_BASE_URL = process.env.API_BASE_URL
+const API_BASE_URL = `${process.env.API_BASE_URL}/${process.env.API_VERSION}`
 const PROVIDER_ADDRESS = process.env.ADDRESS
 const POLL_INTERVAL_MS = parseInt(process.env.POLL_INTERVAL_MS) || 10000 // Default to 1 minute
 const DEADLINE_MINUTES = parseInt(process.env.DEADLINE_MINUTES) || 15 // Default to 15 minutes
@@ -7,6 +7,7 @@ const LOG_IN_MESSAGE =
   'I confirm that I am the sole owner of this wallet address and no other person or entity has access to it. Any transactions that take place under this account are my responsibility and not that of any other. By signing this, I agree to engage in the use of CollarNetworks Marketmaker Bot and I understand that I am responsible for any losses that may occur as a result of my actions.'
 const RPC_URL = process.env.RPC_URL
 const MAX_RETRIES = process.env.MAX_RETRIES || 3
+const CHAIN_ID = process.env.CHAIN_ID
 module.exports = {
   RPC_URL,
   API_BASE_URL,
@@ -16,4 +17,5 @@ module.exports = {
   LOG_IN_MESSAGE,
   MAX_RETRIES,
   BIPS_BASE: 10_000,
+  CHAIN_ID
 }
